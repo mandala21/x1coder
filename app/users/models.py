ae4afa85a9e4d1a401203aeaa3a1c570209522c9
mandala21 <lucasresone@gmail.com>
2020-05-09 19:52:08 -0300

    Init project

---
+++ app/users/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class UserModel(AbstractUser):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
